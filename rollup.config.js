21566394ce1472e6c2eaa79cbb8b7c929340d4fa
chk-m8 <13489983+chk-m8@users.noreply.github.com>
2025-05-25 14:27:54 -0500

    feat(): add delete and copy css functionality

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
+import remove from "@omnihash/rollup-plugin-remove-files";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import copy from "rollup-plugin-copy";
-import dts from "rollup-plugin-dts";
+import { default as dts } from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 
-const packageJson = require("./package.json"); 
+const packageJson = require("./package.json");
 export default [
   {
     input: "src/index.ts",
@@ -30,25 +31,28 @@ export default [
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
       postcss({
-        extract: true, 
+        extract: true,
         minimize: true,
         modules: false,
-        use: ["sass"]
+        use: ["sass"],
       }),
-      copy({
-        targets: [{ src: 'dist/cjs/index.css', dest: 'dist/styles' }],
-        verbose: true,
-        hook: 'buildStart',
-      }), 
     ],
     external: ["react", "react-dom"],
   },
   {
     input: "src/index.ts",
     output: [{ file: "dist/types.d.ts", format: "es" }],
-    plugins: [ 
-      dts.default()
-  ],
-    external: [/\.css$/,/\.scss$/],
+    plugins: [
+      copy({
+        targets: [{ src: "dist/cjs/index.css", dest: "dist/" }],
+        // verbose: true,
+        hook: "buildStart",
+      }),
+      remove({
+        filePaths: ["dist/esm/index.css", "dist/cjs/index.css"],
+      }),
+      dts.default(),
+    ],
+    external: [/\.css$/, /\.scss$/],
   },
 ];
